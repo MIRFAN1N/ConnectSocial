50e90e25032e1355bfc3ec1fa28a6f5d3a0bec48
MIRFAN1N <107563495+MIRFAN1N@users.noreply.github.com>
2022-06-15 12:58:17 -0700

    Updated till 15-June-2022

---
+++ Profile/profile.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile</title>
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.png">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/forgot.css">
+</head>
+<body>
+    <!-- Header -->
+    <div class="page-navigation">
+        <div class="container-fluid">
+            <div class="page-navigation-inner">
+                <ul class="page-navigation-ul">
+                    <li class="page-navigation-li active">
+                        <a href="javascript:void(0)" class="page-navigation-link">About</a>
+                    </li>
+                    <li class="page-navigation-li">
+                        <a href="javascript:void(0)" class="page-navigation-link">Consumer</a>
+                    </li>
+                    <li class="page-navigation-li">
+                        <a href="javascript:void(0)" class="page-navigation-link">Merchants</a>
+                    </li>
+                    <li class="page-navigation-li">
+                        <a href="javascript:void(0)" class="page-navigation-link">Our World</a>
+                    </li>
+                    <li class="page-navigation-li">
+                        <a href="javascript:void(0)" class="page-navigation-link">Treasure Island</a>
+                    </li>
+                </ul>
+                <div class="login-link">
+                    <a href="javascript:void(0)" class="page-navigation-link">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Profile -->
+    <div class="user-profile-div">
+        <div class="container-fluid">
+            <div class="user-profile-div-inner">
+                <div class="user-profile-div-main row">
+                    <div class="col-md-6">
+                        <div class="user-profile-info-inner">
+                            <div class="user-profile-info-image">
+                                <img src="../assets/images/user-profile/user-01.png" alt="">
+                            </div>
+                            <div class="user-profile-info-text">
+                                <h2 class="user-profile-info-text-name">Jane Doe</h2>
+                                <div class="user-profile-info-text-type"><i>Ambassador</i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="user-profile-info-action-inner text-center">
+                            <h4 class="user-profile-info-action-top-text">
+                                Get Started & Join My Network!
+                            </h4>
+                            <div class="user-profile-info-action-buttons">
+                                <button class="user-profile-btn-large white mr-3">Join as Merchant</button>
+                                <button class="user-profile-btn-large black">Join as Ambassador</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="user-profile-about">
+        <div class="container-fluid">
+            <div class="user-profile-about-inner">
+                <div class="user-profile-about-main row">
+                    <div class="user-profile-about-text col-md-5">
+                        <h2 class="user-profile-about-title">
+                            About Ambassador
+                        </h2>
+                        <div class="user-profile-about-text-p">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id libero sit orci sed vel rutrum aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id libero sit orci sed vel rutrum aliquam.<br>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id libero sit orci sed vel rutrum aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+                                Id libero sit orci sed vel rutrum aliquam. Lorem ipsum dolor sit amet, <br>
+                            </p>
+                        </div>
+                        <div class="user-profile-social-info">
+                            <div class="user-profile-social-info-title">
+                                <div class="user-profile-social-info-table">
+                                    <table class="table table-borderd">
+                                        <tbody>
+                                            <tr>
+                                                <td>City</td>
+                                                <td>NYC</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Current State</td>
+                                                <td>NY</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Relationship Status</td>
+                                                <td>Single</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Date of Joining</td>
+                                                <td>28 Jan 2020</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Workplace</td>
+                                                <td>Lorem Ipsum</td>
+                                            </tr>
+                                            <tr>
+                                                <td>High School</td>
+                                                <td>Lorem Ipsum</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="user-profile-about-video col-md-5">
+                        <h2 class="user-profile-about-title">
+                            About Connect Social
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
